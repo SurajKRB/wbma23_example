5f27b02f870116cbafea5a86338a7e6caff2c980
Suraj RanaBhat <Suraj.RanaBhat@metropolia.fi>
2023-02-04 23:55:32 +0200

    Tags and navigation

--- views/Upload.js
+++ views/Upload.js
@@ -9,27 +9,32 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import {useContext, useState} from 'react';
-import {useMedia} from '../hooks/ApiHooks';
+import {useCallback, useContext, useState} from 'react';
+import {useMedia, useTag} from '../hooks/ApiHooks';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {MainContext} from '../contexts/MainContext';
+import {useFocusEffect} from '@react-navigation/native';
+import {appId} from '../utils/variables';
 
 const Upload = ({navigation}) => {
   const [mediaFile, setMediaFile] = useState({});
   const [loading, setLoading] = useState(false);
   const {postMedia} = useMedia();
+  const {postTags} = useTag();
   const {update, setUpdate} = useContext(MainContext);
 
   const {
     control,
     handleSubmit,
     formState: {errors},
+    trigger,
+    reset,
   } = useForm({
     defaultValues: {
       title: '',
       description: '',
     },
-    mode: 'onBlur',
+    mode: 'onChange',
   });
 
   const uploadFile = async (data) => {
@@ -52,20 +57,23 @@ const Upload = ({navigation}) => {
     console.log('form data: ', formData);
 
     try {
-      const result = await postMedia(
-        formData,
-        await AsyncStorage.getItem('userToken')
-      );
-      console.log('upload result: ', result);
+      const token = await AsyncStorage.getItem('userToken');
+      const result = await postMedia(formData, token);
+
+      const appTag = {file_id: result.file_id, tag: appId};
+
+      const tagResult = await postTags(appTag, token);
+      console.log('Tag result: ', tagResult);
 
       Alert.alert('Uploaded', 'File id: ' + result.file_id, [
         {
           text: 'OK',
           onPress: () => {
             console.log('OK Pressed');
-            // TODO: update 'update' state in context
+            // update 'update' state in context
             setUpdate(!update);
-            // TODO: navigate to home page
+            // navigate to home page
+            navigation.navigate('Home');
           },
         },
       ]);
@@ -77,20 +85,37 @@ const Upload = ({navigation}) => {
   };
 
   const pickFile = async () => {
-    // No permissions request is necessary for launching the image library
-    const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 0.5,
-    });
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 0.5,
+      });
 
-    console.log(result);
+      if (!result.canceled) {
+        setMediaFile(result.assets[0]);
 
-    if (!result.canceled) {
-      setMediaFile(result.assets[0]);
+        // validate the form
+        trigger();
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
+
+  const resetForm = () => {
+    setMediaFile({});
+    reset();
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      return () => {
+        resetForm();
+      };
+    }, [])
+  );
 
   return (
     <ScrollView>
@@ -100,7 +125,12 @@ const Upload = ({navigation}) => {
         activeOpacity={1}
       >
         <Card>
-          <Card.Image onPress={pickFile} source={{uri: mediaFile.uri}} />
+          {mediaFile.type === 'video' ? (
+            <Card.Title onPress={pickFile}>Video Selected</Card.Title>
+          ) : (
+            <Card.Image onPress={pickFile} source={{uri: mediaFile.uri}} />
+          )}
+
           <Controller
             control={control}
             rules={{
@@ -125,21 +155,29 @@ const Upload = ({navigation}) => {
 
           <Controller
             control={control}
+            rules={{
+              minLength: {
+                value: 5,
+                message: 'Description min length is 5 characters.',
+              },
+            }}
             render={({field: {onChange, onBlur, value}}) => (
               <Input
                 placeholder="Description"
                 onBlur={onBlur}
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.description && errors.description.message}
               />
             )}
             name="description"
           />
           <Button
-            disabled={!mediaFile.uri}
+            disabled={!mediaFile.uri || errors.title || errors.description}
             title="UPLOAD"
             onPress={handleSubmit(uploadFile)}
           />
+          <Button type="outline" title="Reset" onPress={resetForm} />
           {loading && <ActivityIndicator size="large" />}
         </Card>
       </TouchableOpacity>
