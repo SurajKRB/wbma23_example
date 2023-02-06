d809f896e4f076d2d7ff96a26bdfa630e918ce53
Suraj RanaBhat <Suraj.RanaBhat@metropolia.fi>
2023-02-06 22:59:23 +0200

    starting using video component

--- views/Single.js
+++ views/Single.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {StyleSheet, Text} from 'react-native';
 import PropTypes from 'prop-types';
 import {uploadsUrl} from '../utils/variables';
 import {Card, Icon, ListItem} from '@rneui/themed';
+import {Video} from 'expo-av';
 
 const Single = ({route}) => {
-  const {title, description, filename, time_added: timeAdded} = route.params;
+  const {
+    title,
+    description,
+    filename,
+    time_added: timeAdded,
+    media_type: type,
+  } = route.params;
+
+  const video = useRef(null);
+
   return (
     <Card>
       <Card.Title>{title}</Card.Title>
-      <Card.Image style={styles.image} source={{uri: uploadsUrl + filename}} />
+      <Card.Divider />
+      {type === 'image' ? (
+        <Card.Image
+          style={styles.image}
+          source={{uri: uploadsUrl + filename}}
+          style={{width: '100%', height: '100%'}}
+        />
+      ) : (
+        <Video ref={video} source={{uri: uploadsUrl + filename}} />
+      )}
+
+      <Card.Divider />
       <ListItem>
         <Icon name="schedule"></Icon>
         <ListItem.Title>
